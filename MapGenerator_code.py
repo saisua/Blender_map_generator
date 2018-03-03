bcfbc6a17392a166c842f71d8bbda57a358dad03
saisua <ausias.prieto@gmail.com>
2018-03-03 19:11:24 +0100

    2 errores
    En un if, y durante la asignación de alturas a los puntos del lattice.

--- MapGenerator_code.py
+++ MapGenerator_code.py
@@ -9,15 +9,15 @@ Lattice = [bpy.data.lattices['Lattice']]
 num_lat_side = 2
 
 
-points = Lattice[0].points_u
-Lattice[0].points_v = points
+points_x = Lattice[0].points_u
+points_y = Lattice[0].points_v
 for lat in range(num_lat_side*num_lat_side):
     try:
         lat = str(float(lat)/1000)
         exec("Object_lattice.append(bpy.data.objects['Lattice"+str(lat[1:])+"'])")
         exec("Lattice.append(bpy.data.lattices['Lattice"+str(lat[1:])+"'])")
-        exec("bpy.data.lattices['Lattice"+str(lat[1:])+"'].points_u = points")
-        exec("bpy.data.lattices['Lattice"+str(lat[1:])+"'].points_v = points")
+        exec("bpy.data.lattices['Lattice"+str(lat[1:])+"'].points_u = points_x")
+        exec("bpy.data.lattices['Lattice"+str(lat[1:])+"'].points_v = points_y")
     except:
         print("Lattice"+str(lat)+" doesn't exist")
         pass
@@ -46,11 +46,9 @@ def deform_lattice(min_height, max_height, add_height, oLattice, Lattice, min_la
     for lattices in Lattice:
         Lattice_Vert_matrix.append([])
     #print("Lattice = " + str(math.sqrt(len(Lattice_Vert_matrix))))
-    #Lattice_Vert_x = bpy.types.Lattice(bpy.types.ID(bpy.data.objects['OBJECT']))
     Lattice_Vert_x = Lattice[0].points_u
     Lattice_Vert_y = Lattice[0].points_v
     #print("max vert = (" + str(Lattice_Vert_x) + "," + str(Lattice_Vert_y) +")")
-    #Lattice_Vert_y = Lattice.points()[1]
     contador = -1
     contador_x = -1
     contador_y = 0
@@ -65,7 +63,19 @@ def deform_lattice(min_height, max_height, add_height, oLattice, Lattice, min_la
         contador_x += 1
         print("latt,cont_y,cont_x,cont")
         print(str(latt),str(contador_y),str(contador_x), str(contador))
-        if (len(Lattice_Vert_x_matrix) != 0 and contador == 0) or contador_y-(latt-int(contador_x/Lattice_Vert_x)*Lattice_Vert_y) != 0:
+        ###
+        # El siguiente if no funciona como deberia 
+        # Deberia afectar a todos los puntos que estan entre dos o mas matrices
+        # Osea, dejar pasar si:
+        # -contador < numero de puntos(x)
+        # -contador_x = numero de puntos(x) * numero de lattices(x)
+        # ???-contador_y != (numero de puntos(y) * int(numero de lattices(y)*(cont_latt - 1)) + 1  
+        # ???-contador_y != (numero de puntos(y) * int(numero de lattices(y)/(cont_latt - 1)) + 1  
+        ##
+        
+        if contador_y != (Lattice_Vert_y * int(*(cont_latt - 1)) + 1 and contador_x = numero de puntos(x) * numero de lattices(x) and contador < numero de puntos(x):
+        #if (contador_y-((latt-int(contador_x/Lattice_Vert_x)x)*Lattice_Vert_y) != 0) or (len(Lattice_Vert_x_matrix) != 0 and contador == 0):
+        #if (len(Lattice_Vert_x_matrix) != 0 and contador == 0) or (contador_y-(latt-int(contador_x/Lattice_Vert_x)*Lattice_Vert_y) != 0):
             if (contador_y-(latt-int(contador_x/Lattice_Vert_x))*Lattice_Vert_y) > 0:
                 if (height > Lattice_Vert_matrix[latt][(contador_y-(latt-int(contador_x/Lattice_Vert_x))*Lattice_Vert_y)-1][contador] + add_height or height < Lattice_Vert_matrix[latt][(contador_y-(latt-int(contador_x/Lattice_Vert_x))*Lattice_Vert_y)-1][contador] - add_height):
                     #print("too much height [" + str(Lattice_Vert_matrix[len(Lattice_Vert_matrix)-1][contador]) + "], y")
@@ -83,6 +93,7 @@ def deform_lattice(min_height, max_height, add_height, oLattice, Lattice, min_la
                     #print("new height: " +str(height)) 
             height = height + float(random.randint(-1,1)*add_height)
         else:
+            print("---EXCEPCION---")
             if contador != 0 and len(Lattice_Vert_x_matrix) == 0:
                 height = Lattice_Vert_matrix[latt-1][contador_y-(latt-int(contador_x/Lattice_Vert_x))*Lattice_Vert_y][Lattice_Vert_x-1]
                 if contador_y != 0:
@@ -105,17 +116,22 @@ def deform_lattice(min_height, max_height, add_height, oLattice, Lattice, min_la
             Lattice_Vert_x_matrix = []
             latt += 1
             contador = -1
-            if contador_x >= Lattice_Vert_x*math.sqrt(len(Lattice_Vert_matrix)) - 1:
+            print("")
+            print("--Final de matriz(x)")
+            if contador_x >= Lattice_Vert_x*math.sqrt(len(Lattice_Vert_matrix)) -1:
                 #print("contador_y = " + str(contador_y))
                 contador_x = -1
                 contador_y += 1
+                print("++Final de fila")
                 if contador_y > Lattice_Vert_y*cont_latt:
                     latt_y += int(math.sqrt(len(Lattice_Vert_matrix)))
                     cont_latt += 1
+                    print("######Final de matriz######")
                     #print(latt_y)
                     #print(cont_latt)
                     #time.sleep(1)
                 latt = int(latt_y)
+            print("")
     #print(str(Lattice_Vert_matrix))
     if True:
         print("starting land stat.")
@@ -130,13 +146,17 @@ def deform_lattice(min_height, max_height, add_height, oLattice, Lattice, min_la
         if stad < min_land or stad > max_land:        
             create_map()
         else:
-            for each_lattice in range(len(Lattice_Vert_matrix)-1):
+            for each_lattice in range(len(Lattice_Vert_matrix)):
+                #print("lattice " + str(each_lattice))
                 for height_row in range(len(Lattice_Vert_matrix[each_lattice])-1):
-                    for each_height in range(len(Lattice_Vert_matrix[each_lattice][height_row])-1):
-                        point_x = oLattice[each_lattice].data.points[(height_row-1)*(len(Lattice_Vert_matrix[each_lattice]))+each_height].co_deform[0]
-                        point_y = oLattice[each_lattice].data.points[(height_row-1)*(len(Lattice_Vert_matrix[each_lattice]))+each_height].co_deform[1]
-                        oLattice[each_lattice].data.points[(height_row-1)*(len(Lattice_Vert_matrix[each_lattice]))+each_height].co_deform = (point_x,point_y,Lattice_Vert_matrix[each_lattice][height_row][each_height])
-                        
+                    #print(Lattice_Vert_matrix[each_lattice][height_row])
+                    for each_height in range(len(Lattice_Vert_matrix[each_lattice][height_row])):
+                        #print("modify point["+str(each_height)+ "," + str(height_row)+"] height = " + str(int(Lattice_Vert_matrix[each_lattice][height_row][each_height])))
+                        #print("point[" + str((height_row)*(len(Lattice_Vert_matrix[each_lattice][0])) +each_height) + "]")
+                        point_x = oLattice[each_lattice].data.points[(height_row)*(len(Lattice_Vert_matrix[each_lattice][0]))+each_height].co_deform[0]
+                        point_y = oLattice[each_lattice].data.points[(height_row)*(len(Lattice_Vert_matrix[each_lattice][0]))+each_height].co_deform[1]
+                        oLattice[each_lattice].data.points[(height_row)*(len(Lattice_Vert_matrix[each_lattice][0]))+each_height].co_deform = (point_x,point_y,Lattice_Vert_matrix[each_lattice][height_row][each_height])
+                #print(oLattice[each_lattice])             
             
 def reset_lattice(obLattice,Lattice):
     for each_lattice in Lattice:    
@@ -147,7 +167,7 @@ def reset_lattice(obLattice,Lattice):
  
 def create_map():
     global Object_lattice,Lattice   
-    deform_lattice(-.035,.035,.005,Object_lattice,Lattice,0,100)
+    deform_lattice(-.035*2,.035*2,.005*2,Object_lattice,Lattice,0,100)
     #min_height, max_height, add_height, object_lattice, lattice, min_land, max_land
 
 reset_lattice(Object_lattice,Lattice)
